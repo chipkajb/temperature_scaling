1add256b3cfecf526a98134716c42844cbd1a879
chipkajb <jbc274@cornell.edu>
2024-08-06 13:12:23 -0400

    make it work with my labels

--- temperature_scaling.py
+++ temperature_scaling.py
@@ -1,3 +1,4 @@
+from pdb import set_trace as bp
 import torch
 from torch import nn, optim
 from torch.nn import functional as F
@@ -11,6 +12,7 @@ class ModelWithTemperature(nn.Module):
         NB: Output of the neural network should be the classification logits,
             NOT the softmax (or log softmax)!
     """
+
     def __init__(self, model):
         super(ModelWithTemperature, self).__init__()
         self.model = model
@@ -43,18 +45,17 @@ class ModelWithTemperature(nn.Module):
         logits_list = []
         labels_list = []
         with torch.no_grad():
-            for input, label in valid_loader:
-                input = input.cuda()
-                logits = self.model(input)
+            for sample in valid_loader:
+                logits = self.model(sample["data"].cuda())
                 logits_list.append(logits)
-                labels_list.append(label)
+                labels_list.append(sample["labels"])
             logits = torch.cat(logits_list).cuda()
             labels = torch.cat(labels_list).cuda()
 
         # Calculate NLL and ECE before temperature scaling
         before_temperature_nll = nll_criterion(logits, labels).item()
         before_temperature_ece = ece_criterion(logits, labels).item()
-        print('Before temperature - NLL: %.3f, ECE: %.3f' % (before_temperature_nll, before_temperature_ece))
+        print("Before temperature - NLL: %.3f, ECE: %.3f" % (before_temperature_nll, before_temperature_ece))
 
         # Next: optimize the temperature w.r.t. NLL
         optimizer = optim.LBFGS([self.temperature], lr=0.01, max_iter=50)
@@ -64,13 +65,14 @@ class ModelWithTemperature(nn.Module):
             loss = nll_criterion(self.temperature_scale(logits), labels)
             loss.backward()
             return loss
+
         optimizer.step(eval)
 
         # Calculate NLL and ECE after temperature scaling
         after_temperature_nll = nll_criterion(self.temperature_scale(logits), labels).item()
         after_temperature_ece = ece_criterion(self.temperature_scale(logits), labels).item()
-        print('Optimal temperature: %.3f' % self.temperature.item())
-        print('After temperature - NLL: %.3f, ECE: %.3f' % (after_temperature_nll, after_temperature_ece))
+        print("Optimal temperature: %.3f" % self.temperature.item())
+        print("After temperature - NLL: %.3f, ECE: %.3f" % (after_temperature_nll, after_temperature_ece))
 
         return self
 
@@ -94,6 +96,7 @@ class _ECELoss(nn.Module):
     "Obtaining Well Calibrated Probabilities Using Bayesian Binning." AAAI.
     2015.
     """
+
     def __init__(self, n_bins=15):
         """
         n_bins (int): number of confidence interval bins
@@ -106,7 +109,7 @@ class _ECELoss(nn.Module):
     def forward(self, logits, labels):
         softmaxes = F.softmax(logits, dim=1)
         confidences, predictions = torch.max(softmaxes, 1)
-        accuracies = predictions.eq(labels)
+        accuracies = predictions.eq(torch.where(labels)[1])
 
         ece = torch.zeros(1, device=logits.device)
         for bin_lower, bin_upper in zip(self.bin_lowers, self.bin_uppers):
